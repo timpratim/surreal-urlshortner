703049f5db9f5ca36a25dfcd0c00ddb1e673edb5
Hugh Kaznowski <hugh@surrealdb.com>
2023-04-20 15:23:50 +0100

    Create web layer

--- main.go
+++ main.go
@@ -1,82 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	logger "github.com/sirupsen/logrus"
 	"github.com/timpratim/surreal-urlshortner/repository"
-	"math/rand"
+	"github.com/timpratim/surreal-urlshortner/web"
 	"net/http"
 )
-
-type Result struct {
-	URLs   []URL  `json:"result"`
-	Status string `json:"status"`
-	Time   string `json:"time"`
-}
-
-type URL struct {
-	ID        string `json:"id"`
-	Original  string `json:"original"`
-	Shortened string `json:"shortened"`
-}
-
-var log = logger.New()
 
 const port = 8090
 const url = "ws://localhost:8000/rpc"
 const namespace = "surrealdb-conference-content"
 const database = "urlshortner"
 
-type WebService struct {
-	repository repository.ShortenerRepository
-}
-
-func shortenURL(url string) string {
-	s := ""
-	//rand.Intn(26) returns a random number between 0 and 25. 97 is the ascii value of 'a'. So rand.Intn(26) + 97 returns a random lowercase letter.
-	for i := 0; i < 6; i++ {
-		s += string(rand.Intn(26) + 97)
-	}
-
-	shortendURL := fmt.Sprintf("http://localhost:8090/%s", s)
-	return shortendURL
-}
-
-func (ws *WebService) redirectURL(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Path[1:]
-	log.Tracef("Generating redirect URL for %s", id)
-
-	data, err := ws.repository.FindShortenedURL(id)
-	if err != nil {
-		log.Errorf("failed to query shortened: %+v", err)
-	}
-
-	jsonBytes, err := json.Marshal(data)
-	if err != nil {
-		panic(err)
-	}
-	//unmarshal the data
-	var results []Result
-	err = json.Unmarshal(jsonBytes, &results)
-	if err != nil {
-		panic(err)
-	}
-
-	if len(results) == 0 {
-		panic(errors.New("no results found"))
-	}
-	something := results[0].URLs
-	if len(something) == 0 {
-		panic(errors.New("no results found"))
-	}
-	originalURL := something[0].Original
-	log.Tracef("Original URL: %s", originalURL)
-	//redirect to the original url
-	http.Redirect(w, r, originalURL, http.StatusSeeOther)
-
-}
+var log = logger.New()
 
 func main() {
 	// Create the database repository that uses SurrealDB to store information
@@ -85,31 +22,16 @@ func main() {
 		log.Fatalf("failed to create shortener repository: %+v", err)
 	}
 	log.Infof("Connected to database")
+	// Close connections to the database at program shutdown
 	defer func() {
 		log.Infof("Closing database")
 		repository.Close()
 	}()
 
-	http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
-		original := r.FormValue("url")
-		shortened := shortenURL(original)
-		log.Tracef("shortened url: %s", shortened)
-		//db.Create(&URL{Original: original, Shortened: shortened})
-
-		urlMap, err := repository.CreateShortUrl(original, shortened)
-		if err != nil {
-			log.Fatalf("failed to create short url: %+v", err) // TODO handle this better
-		}
-		log.Tracef("created url mapping: %+v", urlMap)
-
-		// return json response with shortened url
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"shortened": shortened})
-
-	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		redirectURL(db, w, r)
-	})
+	// Create the web service
+	ws := web.NewWebService(repository)
+	http.HandleFunc("/shorten", ws.ShortenURL)
+	http.HandleFunc("/", ws.RedirectURL)
 	log.Infof("Listening on port %d", port)
 	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
---
+++ web/web.go
@@ -0,0 +1,100 @@
+package web
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	logger "github.com/sirupsen/logrus"
+	"github.com/timpratim/surreal-urlshortner/repository"
+	"math/rand"
+	"net/http"
+)
+
+// WebService is used to handle web requests via it's public methods
+type WebService struct {
+	repository *repository.ShortenerRepository
+}
+
+// URL is a representation of a shortened URL
+type URL struct {
+	ID        string `json:"id"`
+	Original  string `json:"original"`
+	Shortened string `json:"shortened"`
+}
+
+// Result is the raw representation of the response from the database
+type Result struct {
+	URLs   []URL  `json:"result"`
+	Status string `json:"status"`
+	Time   string `json:"time"`
+}
+
+var log = logger.New()
+
+func NewWebService(r *repository.ShortenerRepository) *WebService {
+	return &WebService{
+		repository: r,
+	}
+}
+
+func (ws WebService) ShortenURL(writer http.ResponseWriter, request *http.Request) {
+	original := request.FormValue("url")
+	shortened := shortenURL(original)
+	log.Tracef("shortened url: %s", shortened)
+
+	urlMap, err := ws.repository.CreateShortUrl(original, shortened)
+	if err != nil {
+		log.Fatalf("failed to create short url: %+v", err) // TODO handle this better
+	}
+	log.Tracef("created url mapping: %+v", urlMap)
+
+	// return json response with shortened url
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(map[string]string{"shortened": shortened})
+
+}
+
+func (ws WebService) RedirectURL(writer http.ResponseWriter, request *http.Request) {
+	id := request.URL.Path[1:]
+	log.Tracef("Generating redirect URL for %s", id)
+
+	data, err := ws.repository.FindShortenedURL(id)
+	if err != nil {
+		log.Errorf("failed to query shortened: %+v", err)
+	}
+
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	//unmarshal the data
+	var results []Result
+	err = json.Unmarshal(jsonBytes, &results)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(results) == 0 {
+		panic(errors.New("no results found"))
+	}
+	something := results[0].URLs
+	if len(something) == 0 {
+		panic(errors.New("no results found"))
+	}
+	originalURL := something[0].Original
+	log.Tracef("Original URL: %s", originalURL)
+	//redirect to the original url
+	http.Redirect(writer, request, originalURL, http.StatusSeeOther)
+
+}
+
+func shortenURL(url string) string {
+	s := ""
+	//rand.Intn(26) returns a random number between 0 and 25. 97 is the ascii value of 'a'. So rand.Intn(26) + 97 returns a random lowercase letter.
+	for i := 0; i < 6; i++ {
+		s += string(rand.Intn(26) + 97)
+	}
+
+	shortendURL := fmt.Sprintf("http://localhost:8090/%s", s)
+	return shortendURL
+}
